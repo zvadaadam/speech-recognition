7e298e11ca2a3d9b069e1b5dfa53450dcacd13b9
Adam Zvada <zvadaada@gmail.com>
2018-04-04 16:59:34 +0200

    filter unwanted data in dataset labels

--- src/utils/text_utils.py
+++ src/utils/text_utils.py
@@ -1,8 +1,11 @@
 import numpy as np
 import tensorflow as tf
+import re
 
 SPACE_TOKEN = '<space>'
+
 SPACE_INDEX = 0
+
 FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
 
 
@@ -38,7 +41,18 @@ def chars_to_index(char_text):
     return np.asarray([SPACE_INDEX if x == SPACE_TOKEN else ord(x) - FIRST_INDEX for x in char_text])
 
 def simplify_text(text):
-    return ' '.join(text.strip().lower().split(' ')).replace('.', '').replace('?', '').replace(',','').replace( "'", '').replace('!', '').replace('-', '').replace('\n', ' ')
+
+    # lower text
+    simplified_text = ' '.join(text.strip().lower().split(' '))
+
+    # get ride of unwanted characters
+    simplified_text = re.sub(r'([^\s[a-z])+', '', simplified_text)
+
+    # converts multiple whitespaces to one space
+    simplified_text = ' '.join(simplified_text.split())
+
+    return simplified_text
+
 
 def text_to_chars(text):
     # replace space(' ') on two spaces
@@ -51,6 +65,14 @@ def text_to_chars(text):
     refactor_text = np.hstack([SPACE_TOKEN if x == '' else list(x) for x in refactor_text])
 
     return refactor_text
+
+def index_to_text(text_index):
+
+    char_text = ''.join([chr(x) for x in np.asarray(text_index) + FIRST_INDEX])
+    char_text = char_text.replace(chr(ord('z') + 1), '')
+    char_text = char_text.replace(chr(ord('a') - 1), ' ')
+
+    return char_text
 
 def sparse_tuple_from(sequences, dtype=np.int32):
     """Create a sparse representention of x.
@@ -87,3 +109,19 @@ def text_number(num):
          8 : 'eight',
          9 : 'nine'
     }[num]
+
+
+def test():
+
+    text_index = [1,0,19,13,1,12,12,0,14,21,13,2,5,18,0,15,6,0,14,5,23,0,10,15,2,19,0,23,9,12,12,0,1,12,19,15,0,2,5,0,3,18,5,1,20,5,4,-55]
+
+    str = index_to_text(text_index)
+
+    text = 'AHOJ,  jak se \n @ mas 1?'
+
+    print(simplify_text(text))
+
+
+
+if __name__ == "__main__":
+    test()
