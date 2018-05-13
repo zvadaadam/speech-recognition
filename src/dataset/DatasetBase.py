d503eca20da4928e083430d304fd87e947da4115
Adam Zvada <zvadaada@gmail.com>
2018-05-13 12:52:01 +0200

    cached speech features before training

--- src/dataset/DatasetBase.py
+++ src/dataset/DatasetBase.py
@@ -27,6 +27,14 @@ class DatasetBase(object):
         return self._label_filenames
 
     @property
+    def audios(self):
+        return self._audios
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
     def num_examples(self):
         return self._num_examples
 
@@ -47,9 +55,47 @@ class DatasetBase(object):
             self._epochs_completed += 1
 
             # perfrom shuffle
+            files = list(zip(self._audios, self._labels))
+            random.shuffle(files)
+            self._audios, self._labels= zip(*files)
+
+            # start next epoch
+            start = 0
+
+            self._index_in_epoch = batch_size
+
+        end = self._index_in_epoch
+
+        audios_batch = self._audios[start:end]
+        labels_batch = self._labels[start:end]
+
+        output_target = np.asarray(labels_batch)
+        sparse_targets = text_utils.sparse_tuple_from(output_target)
+
+        # pad audio batch
+        train_input, train_length = audio_utils.pad_sequences(audios_batch)
+
+        return train_input, sparse_targets, train_length
+
+
+    def next_batch_and_preprocess(self, batch_size):
+        """" Returns from Datset batch of audios for training/testing of batch_size """
+
+        if batch_size > self._num_examples:
+            raise ValueError('Batch size cannot be greather then number of examples in dataset')
+
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+
+
+        if self._index_in_epoch > self._num_examples:
+            # count finished epoches
+            self._epochs_completed += 1
+
+            # perfrom shuffle
             files = list(zip(self._audio_filenames, self._label_filenames))
             random.shuffle(files)
-            self._audio_filenames, self._label_filenames = zip(*files)
+            self._audio_filenames, self._label_filenames= zip(*files)
 
             # start next epoch
             start = 0
@@ -61,10 +107,9 @@ class DatasetBase(object):
         audios = []
         labels = []
 
-        #print("Preprocessing audio files for batch of size", batch_size)
+        print("Preprocessing audio files for batch of size", batch_size)
 
-        #for audio_filename, label_filename in tqdm(zip(self._audio_filenames[start:end], self._label_filenames[start:end]), total=(end - start)):
-        for audio_filename, label_filename in zip(self._audio_filenames[start:end], self._label_filenames[start:end]):
+        for audio_filename, label_filename in tqdm(zip(self._audio_filenames[start:end], self._label_filenames[start:end]), total=(end - start)):
             audio_features = audio_utils.audiofile_to_input_vector(audio_filename, self.num_features, self.num_context)
 
             text_target = text_utils.get_refactored_transcript(label_filename, is_filename=True, is_digit=False)
@@ -79,5 +124,6 @@ class DatasetBase(object):
         train_input, train_length = audio_utils.pad_sequences(audios)
 
         return train_input, sparse_targets, train_length
+
 
 
--- src/dataset/VCTKDataset.py
+++ src/dataset/VCTKDataset.py
@@ -2,6 +2,7 @@ import os
 from tqdm import tqdm
 
 from src.dataset.DatasetBase import DatasetBase
+from src.utils import audio_utils, text_utils
 
 
 class VCTKDataset(DatasetBase):
@@ -17,7 +18,8 @@ class VCTKDataset(DatasetBase):
         DatasetBase.__init__(self, num_features, num_context)
         self.dataset_path = dataset_path
 
-        self.read_vctk_dataset(num_speakers=3)
+        #self.read_vctk_dataset(num_speakers=3)
+        self.read_and_cache_vctk_dataset(num_speakers=3)
 
 
     def read_vctk_dataset(self, dataset_path=None, num_speakers=None):
@@ -67,6 +69,61 @@ class VCTKDataset(DatasetBase):
         print()
 
         self._num_examples = len(self._audio_filenames)
+
+    def read_and_cache_vctk_dataset(self, dataset_path=None, num_speakers=None):
+
+        if dataset_path is not None:
+            self.dataset_path = dataset_path
+
+        print("Retriving all filenames for VCTK training dataset from path", self.dataset_path)
+
+        audio_dataset_path = os.path.join(self.dataset_path, 'wav48')
+        label_dataset_path = os.path.join(self.dataset_path, 'txt')
+
+        # gets list of directories for diffrent speakers
+        speakers_dirs = os.listdir(audio_dataset_path)
+
+        if num_speakers is not None:
+            speakers_dirs = speakers_dirs[0:num_speakers]
+            print('Number of speakers: ', num_speakers)
+        else:
+            print('All speakers')
+
+        for speaker_dir in tqdm(speakers_dirs, total=len(speakers_dirs)):
+
+            # get full paths for speakers
+            speaker_audio_path = os.path.join(audio_dataset_path, speaker_dir)
+            speaker_label_path = os.path.join(label_dataset_path, speaker_dir)
+
+            # ignore inconsistency in data or anything that is not directory
+            if not os.path.isdir(speaker_audio_path) or not os.path.isdir(speaker_label_path):
+                continue
+
+            # Getting full paths to all audios and labels of the speaker
+            audio_paths = [os.path.join(speaker_audio_path, speaker_filename) for speaker_filename in os.listdir(speaker_audio_path)]
+            label_paths = [os.path.join(speaker_label_path, speaker_filename) for speaker_filename in os.listdir(speaker_label_path)]
+
+            audio_paths.sort()
+            label_paths.sort()
+
+            # concatenate speaker filenames
+            self._audio_filenames = self._audio_filenames + audio_paths
+            self._label_filenames = self._label_filenames + label_paths
+
+
+        self._num_examples = len(self._audio_filenames)
+
+        self._audios = []
+        self._labels = []
+
+        for audio_filename, label_filename in tqdm(zip(self._audio_filenames, self._label_filenames), total=self._num_examples):
+            audio_features = audio_utils.audiofile_to_input_vector(audio_filename, self.num_features, self.num_context)
+
+            text_target = text_utils.get_refactored_transcript(label_filename, is_filename=True, is_digit=False)
+
+            self._audios.append(audio_features)
+            self._labels.append(text_target)
+
 
 
 def test_dataset():
