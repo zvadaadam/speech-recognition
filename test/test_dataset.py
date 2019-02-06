78d676cfebcc0935923fd78bdb24515fd6577f9c
zvadaadam <zvadaada@gmail.com>
2019-02-06 19:43:22 +0100

    init tests

---
+++ test/test_dataset.py
@@ -0,0 +1,7 @@
+
+
+def test_read_digit_dataset():
+    pass
+
+def test_read_vctk_dataset():
+    pass
---
+++ test/test_trainer.py
@@ -0,0 +1,8 @@
+import pytest
+from click.testing import CliRunner
+
+def test_run_training():
+
+    runner = CliRunner()
+
+    #result = runner.invoke(todo_run, ['push', '--force'])
